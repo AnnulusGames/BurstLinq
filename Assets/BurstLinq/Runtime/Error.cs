a6de5715a68d5b15bdebfb5e89551cd53d54b6a2
AnnulusGames <annulusgames@gmail.com>
2023-12-10 15:38:58 +0900

    Add: AggressiveInlining to Error methods

--- Assets/BurstLinq/Runtime/Error.cs
+++ Assets/BurstLinq/Runtime/Error.cs
@@ -1,19 +1,23 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace BurstLinq
 {
     internal static class Error
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ThrowIfNull<T>(T obj)
         {
             if (obj == null) throw new ArgumentNullException();
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ThrowIfEmpty(int length)
         {
             if (length <= 0) throw new InvalidOperationException("Sequence contains no elements");
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ThrowIfEmpty(bool isEmpty)
         {
             if (isEmpty) throw new InvalidOperationException("Sequence contains no elements");
